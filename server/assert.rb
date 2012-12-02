04d27e1fb63c0262b0cc8d1d9559d4415515bf0e
Christopher Alfeld <calfeld@calfeld.net>
2012-12-02 14:53:53 -0600

    All: License under Apache 2.0 license.

--- server/assert.rb
+++ server/assert.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/env ruby1.9
 
+# Copyright 2010-2012 Christopher Alfeld
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Very basic assertion library.
 #
 # This library exists to provide basic assertion support for Heron without
@@ -12,7 +26,6 @@
 #   assert_throw( "x did not throw" ) { foo() }
 #
 # Author::    Christopher Alfeld (calfeld@calfeld.net)
-# Copyright:: Copyright (c) 2012 Christopher Alfeld
 
 # Throw exception with given message if block evaluates to false.
 def assert( message = "Assert Fail" )
--- server/comet.rb
+++ server/comet.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/env ruby1.9
 
+# Copyright 2010-2012 Christopher Alfeld
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Underlying server-side support for Heron.Comet.
 #
 # This file defines Heron::Comet, a class to do the server side state
@@ -11,7 +25,6 @@
 # Running this file directly will execute some basic unit tests.
 #
 # Author::    Christopher Alfeld (calfeld@calfeld.net)
-# Copyright:: Copyright (c) 2010-2012 Christopher Alfeld
 
 require 'set'
 require 'thread'
